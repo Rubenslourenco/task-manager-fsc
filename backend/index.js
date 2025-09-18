23f5981924b7cc709e446cd7f0d8c25c9afff151
Rubenslourenco <alvesrubens926@gmail.com>
2025-09-17 22:55:16 -0300

    feat: add router file to task entity

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import TaskRouter from "./src/routes/task.route.js";
 
 import connectToDatabase from "./src/database/mongoose.database.js";
-import TaskModel from "./src/models/task.models.js";
-
 
 
 dotenv.config();
@@ -12,73 +11,6 @@ app.use(express.json());
 
 connectToDatabase();
 
-
-app.get("/tasks", async (req, res) => {
-    try {
-        const tasks = await TaskModel.find({});
-        res.status(200).json(tasks);
-    } catch (error) {
-        res.status(500).json({ error: "Erro ao buscar tarefas" });
-    }
-});
-
-app.get("/tasks/:id", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const task = await TaskModel.findById(taskId);
-        res.status(200).json(task);
-    } catch (error) {
-        console.log(error);
-    }
-})
-
-app.post("/tasks", async (req, res) => {
-
-    try {
-        const newtask = new TaskModel(req.body);
-        await newtask.save();
-        res.status(201).send(newtask);
-    } catch (error) {
-        res.status(500).json({ error: "Erro ao criar tarefa" });    
-    }
-   
-});
-
-app.patch('/tasks/:id', async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const taskData = req.body;
-
-        const taskTpUpdate = await TaskModel.findById(taskId)
-
-        const allowedUpdates = ['isCompleted'];
-        const requestedUpdates = Object.keys(taskData);
-
-        for(const update of requestedUpdates) {
-            if(allowedUpdates.includes(update)) {
-                taskTpUpdate[update] = taskData[update];
-        } else {
-            return res.status(500).send({ error: 'Atualização inválida!' });
-        }
-    }
-
-      await taskTpUpdate.save();
-      return  res.status(200).send(taskTpUpdate);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "Erro ao atualizar tarefa" });
-    }
-});
-
-app.delete("/tasks/:id", async (req, res) => {
-    try {
-        const  id  = req.params.id;
-        const deletedTask = await TaskModel.findByIdAndDelete(id);
-        res.status(200).send(deletedTask);
-    } catch (error) {
-        
-        res.status(500).json({ error: "Erro ao deletar tarefa" });
-    }
-});
+app.use('/tasks', TaskRouter);
 
 app.listen(3000);
---
+++ backend/src/routes/task.route.js
@@ -0,0 +1,74 @@
+import express from"express";
+import TaskModel from"../models/task.models.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    try {
+        const tasks = await TaskModel.find({});
+        res.status(200).json(tasks);
+    } catch (error) {
+        res.status(500).json({ error: "Erro ao buscar tarefas" });
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const task = await TaskModel.findById(taskId);
+        res.status(200).json(task);
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.post("/", async (req, res) => {
+
+    try {
+        const newtask = new TaskModel(req.body);
+        await newtask.save();
+        res.status(201).send(newtask);
+    } catch (error) {
+        res.status(500).json({ error: "Erro ao criar tarefa" });    
+    }
+   
+});
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const taskData = req.body;
+
+        const taskTpUpdate = await TaskModel.findById(taskId)
+
+        const allowedUpdates = ['isCompleted'];
+        const requestedUpdates = Object.keys(taskData);
+
+        for(const update of requestedUpdates) {
+            if(allowedUpdates.includes(update)) {
+                taskTpUpdate[update] = taskData[update];
+        } else {
+            return res.status(500).send({ error: 'Atualização inválida!' });
+        }
+    }
+
+      await taskTpUpdate.save();
+      return  res.status(200).send(taskTpUpdate);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Erro ao atualizar tarefa" });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const  id  = req.params.id;
+        const deletedTask = await TaskModel.findByIdAndDelete(id);
+        res.status(200).send(deletedTask);
+    } catch (error) {
+        
+        res.status(500).json({ error: "Erro ao deletar tarefa" });
+    }
+});
+
+export default router;
