e230deea1fcb504074a1f2c7487596f50ae8f116
Rubenslourenco <alvesrubens926@gmail.com>
2025-11-04 23:11:05 -0300

    feat: add function to addd a task

--- frontend/src/components/AddTask.jsx
+++ frontend/src/components/AddTask.jsx
@@ -1,7 +1,11 @@
-import  CustomInput  from './CustomInput';
+import { useState } from 'react';
+import {FaPlus} from 'react-icons/fa';
+import axios from 'axios';
 
 import './AddTask.scss';
-import { useState } from 'react';
+
+import CustomButton from './CustomButton';
+import  CustomInput  from './CustomInput';
 
 const AddTask = () => {
     const [task, setTask] = useState("")
@@ -9,11 +13,27 @@ const AddTask = () => {
     const onChange = (e) => {
         setTask(e.target.value)
     }
-    
+
+    const handleTaskAdd =  async () => {
+        try {
+            if(task.length === 0) {
+                return "Tarefa nao pode ser vazia";
+                
+            }
+            await axios.post("http://localhost:3000/tasks", {
+                description: task,
+                isCompleted: false
+            });
+        } catch (error) {
+            
+        }
+    }
 
     return ( 
         <div className="add-task-container">
             <CustomInput label="Adicionar tarefa..." value={task} onChange={onChange} />
+            <CustomButton><FaPlus size={14} onClick={handleTaskAdd} color='white'/></CustomButton>
+
         </div>
      );
 }
