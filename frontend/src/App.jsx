92850491f9c12e1b2d0db45eee7271ccd26b54a5
Rubenslourenco <alvesrubens926@gmail.com>
2025-11-14 17:05:53 -0300

    feat: add HookUseReduce component and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import HookUseState from './hook/HookUseState';
 import HookUseEffect from './hook/HookUseEffect';
 import HookUseRef from './hook/HookUseRef';
+import HookUseReduce from './hook/HookUseReduce';
 
 import './App.scss';
 
@@ -17,6 +18,7 @@ const App = () => {
       <Route path="/hook-usestate" element={<HookUseState />} />
       <Route path="/hook-useeffect" element={<HookUseEffect />} />
       <Route path="/hook-useref" element={<HookUseRef />} />
+      <Route path="/hook-usereduce" element={<HookUseReduce />} />
     </Routes>
   );
 }
---
+++ frontend/src/hook/HookUseReduce.jsx
@@ -0,0 +1,27 @@
+import { useReducer } from "react";
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "increment":
+            return { counter: state.counter + 1 };
+        case "decrement":
+            return {counter: state.counter - 1 };
+    
+    default:
+        return state;
+}
+}
+
+
+const HookUseReduce = () => {
+    const [state, dispatch] = useReducer(reducer, { counter: 0 });
+    return (
+    <div>
+            <p>{state.counter}</p>
+            <button onClick={() => dispatch({type: "increment"})}>Increment</button>
+            <button onClick={() => dispatch({type: "decrement"})} >Decrement</button>
+
+    </div> );
+}
+ 
+export default HookUseReduce;
